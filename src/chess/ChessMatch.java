72b24fe54eacd3f6c058646e7466e4a9e1d637cc
uevitontech <uevitonsantos@outlook.com>
2021-01-29 13:19:34 -0300

    Special move - En Passant

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -113,6 +113,7 @@ public class ChessMatch {
 			capturedPieces.add(capturedPiece);
 		}
 
+<<<<<<< HEAD
 		// #specialmove castling kingside rook
 
 		if (p instanceof King && target.getColumn() == source.getColumn() + 2) {
@@ -148,6 +149,8 @@ public class ChessMatch {
 			}
 		}
 
+=======
+>>>>>>> parent of 546def8 (Special move - castling)
 		return capturedPiece;
 	}
 
@@ -161,6 +164,7 @@ public class ChessMatch {
 			capturedPieces.remove(capturedPiece);
 			piecesOnTheBoard.add(capturedPiece);
 		}
+<<<<<<< HEAD
 
 		// #special move castling kingside rook
 
@@ -195,6 +199,8 @@ public class ChessMatch {
 				board.placePiece(pawn, pawnPosition);
 			}
 		}
+=======
+>>>>>>> parent of 546def8 (Special move - castling)
 	}
 
 	private void validateSourcePosition(Position position) {
@@ -284,7 +290,7 @@ public class ChessMatch {
 		placeNewPiece('b', 1, new Knight(board, Color.WHITE));
 		placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
 		placeNewPiece('d', 1, new Queen(board, Color.WHITE));
-		placeNewPiece('e', 1, new King(board, Color.WHITE, this));
+		placeNewPiece('e', 1, new King(board, Color.WHITE));
 		placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
 		placeNewPiece('g', 1, new Knight(board, Color.WHITE));
 		placeNewPiece('h', 1, new Rook(board, Color.WHITE));
@@ -301,7 +307,7 @@ public class ChessMatch {
 		placeNewPiece('b', 8, new Knight(board, Color.BLACK));
 		placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
 		placeNewPiece('d', 8, new Queen(board, Color.BLACK));
-		placeNewPiece('e', 8, new King(board, Color.BLACK, this));
+		placeNewPiece('e', 8, new King(board, Color.BLACK));
 		placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
 		placeNewPiece('g', 8, new Knight(board, Color.BLACK));
 		placeNewPiece('h', 8, new Rook(board, Color.BLACK));
